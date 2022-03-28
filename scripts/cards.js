2aed73935282c0068c89398cd46ecdbbf3ca0794
Vladislav Noskov <noskov.vlad@mail.ru>
2022-03-28 22:06:04 +0300

    Add files via upload

---
+++ scripts/cards.js
@@ -0,0 +1,27 @@
+// массив 6 карточек 
+const initialCards = [
+  {
+    name: 'Ординская пещера, Пермский край',
+    link: 'https://static.tildacdn.com/tild3931-6539-4230-b733-383936376339/03_ordinskaya_pesher.jpg'
+  },
+  {
+    name: 'Чарские пески, Забайкалье',
+    link: 'https://static.tildacdn.com/tild6237-3434-4061-b334-633431633834/05_charskie_peski.jpg'
+  },
+  {
+    name: 'Гамсутль, Дагестан',
+    link: 'https://static.tildacdn.com/tild6530-6331-4263-b833-653831646134/12_Gamstul.jpg'
+  },
+  {
+    name: 'Мраморный каньон Рускеала, Карелия',
+    link: 'https://static.tildacdn.com/tild6136-6366-4262-a531-393631633931/13_Ruskeala.jpg'
+  },
+  {
+    name: 'Розовое озеро Сасык-Сиваш, Крым',
+    link: 'https://static.tildacdn.com/tild6535-3937-4331-b732-336136616164/11_Sasyk-sivash.jpg'
+  },
+  {
+    name: 'Болгар. Татарстан',
+    link: 'https://static.tildacdn.com/tild3963-6363-4330-b031-393239356436/18_Bolgar.jpg'
+  }
+];
